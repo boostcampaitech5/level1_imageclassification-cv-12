cb135782da34f7e06a02134f84a01eed95a5d90d
gndldl <jhl9804@naver.com>
2023-04-17 06:46:57 +0000

    .

--- baseline/model.py
+++ baseline/model.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-
+import torchvision.models as models
 
 class BaseModel(nn.Module):
     def __init__(self, num_classes):
@@ -37,16 +37,10 @@ class BaseModel(nn.Module):
 class MyModel(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
+        self.model = models.resnet101(pretrained=True)
+        num_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_features, num_classes)
 
-        """
-        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
-        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
-        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
-        """
-
+    
     def forward(self, x):
-        """
-        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
-        2. 결과로 나온 output 을 return 해주세요
-        """
-        return x
+        return self.model(x)
